769e719a9367c5e8c30c5c552e1e5640dbbfb997
Jessi <jessivelazq1@gmail.com>
2021-07-16 16:39:18 -0700

    completed Auth, scaffold, Redux Store, and Profile Page

--- src/components/loginbutton.js
+++ src/components/loginbutton.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Zen Loop',
+      'cursive',
+    ].join(','),
+  },});
 
 const useStyles = makeStyles((theme) => ({
   loginButton: {
@@ -10,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
     borderWidth: 1,
     borderColor: 'gray',
     borderStyle: 'solid',
+    fontSize: 20,
   },
 }));
 
@@ -22,20 +32,24 @@ function LoginButton(props) {
   } = useAuth0();
 
   return isAuthenticated ? (
-    <Button
-      className={classes.loginButton}
-      onClick={() => logout({ returnTo: window.location.origin })}
-    >
-      Log Out
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button
+        className={classes.loginButton}
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Log Out
+      </Button>
+    </ThemeProvider>
   ) : (
-    <Button 
-      type="button" 
-      className={classes.loginButton}
-      onClick={() => loginWithRedirect()}
-    >
-      Log In
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button 
+        type="button" 
+        className={classes.loginButton}
+        onClick={() => loginWithRedirect()}
+      >
+        Log In
+      </Button>
+    </ThemeProvider>
   );
 }
 
