0a8e5d423902ac3b48a133f204f1862fb463beb8
Jessi <jessivelazq1@gmail.com>
2021-08-10 09:29:58 -0700

    added search by activity - both dropdowns showing same list, prob need to recreate anchorEl stuff for activities

---
+++ src/store/actCodes.js
@@ -0,0 +1,25 @@
+let initialState = {
+  selectedActivity: ''
+}
+
+//=========REDUCER============\\
+export default (state = initialState, action) => {
+  let { type, payload } = action;
+
+  switch(type) {
+    case 'SELECT ACT':
+      console.log({payload});
+      return { ...state, selectedActivity: payload}
+
+      default:
+        return state;
+  }
+}
+
+//=========ACTIONS============\\
+export const selectActivity = (activity) => {
+  return {
+    type: 'SELECT ACT',
+    payload: activity
+  }
+}
